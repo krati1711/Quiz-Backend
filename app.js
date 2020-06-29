70190066d3ca8828b0e5511877d7c220742c8aee
krati1711 <kratimathur142@gmail.com>
2020-06-29 19:43:56 +0530

    connection not open

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/adminRoutes');
+
+app.use(bodyParser.json()); 
+
+/*app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });*/
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+  });
+
+app.use(authRoutes);
+app.use('/ll', (req, res, next) => {
+    console.log("ll me aaya");
+});
+  
+  mongoose
+    .connect(
+      'mongodb+srv://krati:krati@cluster0-vccpc.mongodb.net/Quiz?retryWrites=true&w=majority'
+    )
+    .then(result => {
+        console.log("res->" + result);
+
+      app.listen(5000);
+    })
+    .catch(err => console.log("err->"+err));
+    //app.listen(5000);
+  
+
+// app.listen(3000)
+
+ 
